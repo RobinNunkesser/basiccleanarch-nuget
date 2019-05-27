be529a49bc3c60150d760c944351f6659f28a6bf
Robin Nunkesser <robin.nunkesser@hshl.de>
2019-05-27 10:12:11 +0200

    Version 0.2.0

---
+++ BasicCleanArch/SectionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.ObjectModel;
+
+namespace BasicCleanArch
+{
+    /// <summary>
+    /// Covenience `ViewModel` for Group bindings in ListViews.
+    /// </summary>
+    public class SectionViewModel<T> : ObservableCollection<T>
+    { 
+        public string LongName { get; set; }
+        public string ShortName { get; set; }
+    }
+}
---
+++ BasicCleanArch/ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BasicCleanArch
+{
+    /// <summary>
+    /// An abstract `ViewModel` superclass that contains an implementation of
+    /// INotifyPropertyChanged.
+    /// </summary>
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Should be called when a property changes.
+        /// </summary>
+        /// <param name="name">Optional: property name.</param>
+        public void OnPropertyChanged([CallerMemberName] string name = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        #endregion
+    }
+}
