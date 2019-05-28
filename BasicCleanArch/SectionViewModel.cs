53795f33b73d30af345df70a3860bdd313353b46
Robin Nunkesser <robin.nunkesser@hshl.de>
2019-05-28 13:13:10 +0200

    Version 0.3.0

--- BasicCleanArch/SectionViewModel.cs
+++ BasicCleanArch/SectionViewModel.cs
@@ -1,13 +1,41 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace BasicCleanArch
 {
     /// <summary>
-    /// Covenience `ViewModel` for Group bindings in ListViews.
+    ///     Convenience `ViewModel` for Group bindings in ListViews.
     /// </summary>
     public class SectionViewModel<T> : ObservableCollection<T>
-    { 
-        public string LongName { get; set; }
-        public string ShortName { get; set; }
+    {
+        private string _longName;
+        private string _shortName;
+
+        public string LongName
+        {
+            get => _longName;
+            set
+            {
+                if (_longName != value)
+                {
+                    _longName = value;
+                    OnPropertyChanged(new PropertyChangedEventArgs("LongName"));
+                }
+            }
+        }
+
+        public string ShortName
+        {
+            get => _shortName;
+            set
+            {
+                if (_shortName != value)
+                {
+                    _shortName = value;
+                    OnPropertyChanged(
+                        new PropertyChangedEventArgs("ShortName"));
+                }
+            }
+        }
     }
 }
